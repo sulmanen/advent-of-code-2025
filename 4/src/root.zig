52f4db25743efd65b89580e47ea1a61a356becd2
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2025-12-14 22:34:24 +0200

    day 4 runs but incorrect answer

--- 4/src/root.zig
+++ 4/src/root.zig
@@ -4,6 +4,10 @@ const Rectangle = struct {
     width: usize,
     height: usize,
 };
+const CoOrdinate = struct {
+    row: i64,
+    column: i64,
+};
 const GRID_WIDTH: usize = 10;
 const GRID_HEIGHT: usize = 10;
 
@@ -43,11 +47,44 @@ pub fn populatePapers(filename: []const u8, grid: *[GRID_WIDTH][GRID_HEIGHT]bool
     }
 }
 
-pub fn forkLiftCanAccess(grid: *[GRID_HEIGHT][GRID_WIDTH]bool, rowIndex: usize, columnIndex: usize) bool {
-    if (grid[rowIndex][columnIndex]) {
-        return true;
+pub fn forkLiftCanAccess(grid: *[GRID_HEIGHT][GRID_WIDTH]bool, row_index: usize, column_index: usize) bool {
+    var has_paper: usize = 0;
+    const top_left: CoOrdinate = .{ .row = if (row_index > 0) @intCast(row_index - 1) else -1, .column = if (column_index > 0) @intCast(column_index - 1) else -1 };
+    const top: CoOrdinate = .{ .row = if (row_index > 0) @intCast(row_index - 1) else -1, .column = @intCast(column_index) };
+    const top_right: CoOrdinate = .{ .row = if (row_index > 0) @intCast(row_index - 1) else -1, .column = @intCast(column_index + 1) };
+    const right: CoOrdinate = .{ .row = @intCast(row_index), .column = @intCast(column_index + 1) };
+    const bottom_right: CoOrdinate = .{ .row = @intCast(row_index + 1), .column = @intCast(column_index + 1) };
+    const bottom: CoOrdinate = .{ .row = @intCast(row_index + 1), .column = @intCast(column_index) };
+    const bottom_left: CoOrdinate = .{ .row = @intCast(row_index + 1), .column = if (column_index > 0) @intCast(column_index - 1) else -1 };
+    const left: CoOrdinate = .{ .row = @intCast(row_index), .column = if (column_index > 0) @intCast(column_index - 1) else -1 };
+
+    if (grid[row_index][column_index]) {
+        if (top_left.row < 0 and top_left.row < GRID_HEIGHT and top_left.column < 0 and top_left.column < GRID_WIDTH and grid[@intCast(top_left.row)][@intCast(top_left.column)]) {
+            has_paper += 1;
+        }
+        if (top.row < 0 and top.row < GRID_HEIGHT and top.column < 0 and top.column < GRID_WIDTH and grid[@intCast(top.row)][@intCast(top.column)]) {
+            has_paper += 1;
+        }
+        if (top_right.row < 0 and top_right.row < GRID_HEIGHT and top_right.column < 0 and top_right.column < GRID_WIDTH and grid[@intCast(top_right.row)][@intCast(top_right.column)]) {
+            has_paper += 1;
+        }
+        if (right.row < 0 and right.row < GRID_HEIGHT and right.column < 0 and right.column < GRID_WIDTH and grid[@intCast(right.row)][@intCast(right.column)]) {
+            has_paper += 1;
+        }
+        if (bottom_right.row < 0 and bottom_right.row < GRID_HEIGHT and bottom_right.column < 0 and bottom_right.column < GRID_WIDTH and grid[@intCast(bottom_right.row)][@intCast(bottom_right.column)]) {
+            has_paper += 1;
+        }
+        if (bottom.row < 0 and bottom.row < GRID_HEIGHT and bottom.column < 0 and bottom.column < GRID_WIDTH and grid[@intCast(bottom.row)][@intCast(bottom.column)]) {
+            has_paper += 1;
+        }
+        if (bottom_left.row < 0 and bottom_left.row < GRID_HEIGHT and bottom_left.column < 0 and bottom_left.column < GRID_WIDTH and grid[@intCast(bottom_left.row)][@intCast(bottom_left.column)]) {
+            has_paper += 1;
+        }
+        if (left.row < 0 and left.row < GRID_HEIGHT and left.column < 0 and left.column < GRID_WIDTH and grid[@intCast(left.row)][@intCast(left.column)]) {
+            has_paper += 1;
+        }
     }
-    return false;
+    return has_paper < 4;
 }
 
 pub fn solveOne(filename: []const u8) !void {
