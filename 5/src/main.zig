c3b129a525296b79a1862dc86a6794de9d1e0fda
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2025-12-20 14:42:36 +0200

    turn on day 5 solver for first problem

--- 5/src/main.zig
+++ 5/src/main.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 const _5 = @import("_5");
 
 pub fn main() !void {
-    //try _5.solveOne("input.txt");
+    try _5.solveOne("input.txt");
     try _5.solveTwo("input.txt");
 }
