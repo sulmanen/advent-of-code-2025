a5b060bdc6eef30e253012c68a5f4528de4b9819
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2025-12-21 16:56:01 +0200

    day 6.2 save the current situation which runs

--- 6/src/main.zig
+++ 6/src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const _6 = @import("_6");
 
 pub fn main() !void {
-    const one: u128 = try _6.solveOne("input.txt");
-    std.debug.print("First Total: {d}\n", .{one});
+    //const one: u128 = try _6.solveOne("input.txt");
+    //std.debug.print("First Total: {d}\n", .{one});
+
+    const two: u128 = try _6.solveTwo("input.txt");
+    std.debug.print("Second Total: {d}\n", .{two});
 }
--- 6/src/root.zig
+++ 6/src/root.zig
@@ -48,3 +48,80 @@ pub fn solveOne(input: []const u8) !u128 {
     }
     return total;
 }
+
+pub fn addCephalopod(numbers: *[][]const u8) !u128 {
+    var max_length: usize = 0;
+    var j: usize = 0;
+    while (j < numbers.len) : (j += 1) {
+        max_length = @max(max_length, numbers.*[j].len);
+    }
+    for (0..numbers.len) |i| {
+        const number = numbers.*[i];
+        const padded_num = try std.fmt.allocPrint(std.heap.page_allocator, "{s:0>[1]}", .{ number, max_length });
+        defer std.heap.page_allocator.free(padded_num);
+    }
+    return 0;
+}
+
+pub fn multiplyCephalopod(numbers: *[][]const u8) !u128 {
+    var max_length: usize = 0;
+    for (numbers.*) |number| {
+        max_length = @max(max_length, number.len);
+    }
+    for (0..numbers.len) |i| {
+        const number = numbers.*[i];
+        const padded_num = try std.fmt.allocPrint(std.heap.page_allocator, "{s:0>[1]}", .{ number, max_length });
+        defer std.heap.page_allocator.free(padded_num);
+    }
+    return 0;
+}
+
+pub fn solveTwo(input: []const u8) !u128 {
+    const file = try std.fs.cwd().openFile(input, .{});
+    defer file.close();
+    var file_buffer: [4096]u8 = undefined;
+    var reader = file.reader(&file_buffer);
+
+    var operators: std.ArrayList([]const u8) = try std.ArrayList([]const u8).initCapacity(std.heap.page_allocator, 50);
+    defer operators.deinit(std.heap.page_allocator);
+
+    var operands = try std.ArrayList([]const u8).initCapacity(std.heap.page_allocator, 50);
+    defer operands.deinit(std.heap.page_allocator);
+    var width: usize = 0;
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        const right_trimmed = std.mem.trimRight(u8, line, " \t\n\r");
+
+        if (std.mem.startsWith(u8, right_trimmed, "*") or std.mem.startsWith(u8, right_trimmed, "+")) {
+            var iter = std.mem.tokenizeAny(u8, right_trimmed, " \t\n\r");
+            while (iter.next()) |operator| : (width += 1) {
+                try operators.append(std.heap.page_allocator, operator);
+            }
+        } else {
+            var iter = std.mem.tokenizeAny(u8, right_trimmed, " \t\n\r");
+            while (iter.next()) |operand| {
+                try operands.append(std.heap.page_allocator, operand);
+            }
+        }
+    }
+    var total: u128 = 0;
+    const row_count: usize = operands.items.len / width;
+    for (operators.items, 0..) |operator, i| {
+        if (std.mem.eql(u8, operator, "+")) {
+            var numbers = try std.heap.page_allocator.alloc([]const u8, row_count);
+            defer std.heap.page_allocator.free(numbers);
+            for (0..row_count) |j| {
+                numbers[j] = operands.items[j * width + i];
+            }
+            total += try addCephalopod(&numbers);
+        }
+        if (std.mem.eql(u8, operator, "*")) {
+            var numbers = try std.heap.page_allocator.alloc([]const u8, row_count);
+            defer std.heap.page_allocator.free(numbers);
+            for (0..row_count) |j| {
+                numbers[j] = operands.items[j * width + i];
+            }
+            total += try multiplyCephalopod(&numbers);
+        }
+    }
+    return total;
+}
